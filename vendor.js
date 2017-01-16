77e708d5bba2993eada4cd6e85269b66ced89b04
dineshdjadhav94 <dinesh.jadhav@deltabee.com>
2017-01-16 12:37:47 +0530

    Email Send(Vendor)

--- vendor.js
+++ vendor.js
@@ -1,37 +1,59 @@
-exports.addVendor = function(pool){
-  return function(req,res){
-    res.setHeader('Content-Type', 'application/json');  
-    var vandor_name = req.body.vendor_name;  
-    var email = req.body.email;
-    var contact =  req.body.contact;
-    var id_proof =  req.body.id_proof;
-    var block_id =  req.body.block_id;
-    var description = req.body.description;  
+exports.addVendor = function(pool,transporter){
+    return function(req,res){
+        res.setHeader('Content-Type', 'application/json');  
+        var vandor_name = req.body.vendor_name;  
+        var email = req.body.email;
+        var contact =  req.body.contact;
+        var id_proof =  req.body.id_proof;
+        var block_id =  req.body.block_id;
+        var description = req.body.description;  
 
-    var result = {}
-    var queryString = "select * from image_temp where id='"+id_proof+"'";
-    pool.query(queryString, function(err, rows, fields) {
-        if (err){
-            result.error= err;
-            res.send(JSON.stringify(result)); 
-        }else{
-            if (rows.length>0) {
-                var id_imageName = rows[0].imgName;
-                var Q = 'INSERT INTO vendor_master ( `vendor_name`, `email`, `contact`, `id_poof`, `status`, `block_id`, `description`) VALUES ("'+vandor_name+'","'+email+'","'+contact+'","'+id_imageName+'","1", "'+block_id+'", "'+description+'")';
-                pool.query(Q, function(err, rows, fields) {
-                    var query = "delete from image_temp where id='"+id_proof+"'";
-                    pool.query(query, function(err, rows, fields) {
-                        result.status = 200;
-                        res.send(JSON.stringify(result)); 
-                    });
-                });
-            }else{
-                result.error = "Select ID Image."
+        var result = {}
+        var queryString = "select * from image_temp where id='"+id_proof+"'";
+            pool.query(queryString, function(err, rows, fields) {
+            if (err){
+                result.error= err;
                 res.send(JSON.stringify(result)); 
+                }else{
+                if (rows.length>0) {
+                    var id_imageName = rows[0].imgName;
+                    var Q = 'INSERT INTO vendor_master ( `vendor_name`, `email`, `contact`, `id_poof`, `status`, `block_id`, `description`) VALUES ("'+vandor_name+'","'+email+'","'+contact+'","'+id_imageName+'","1", "'+block_id+'", "'+description+'")';
+                    pool.query(Q, function(err, rows, fields) {
+                        if(err){
+                            console.log(err);
+                        }else{
+                        transporter.sendMail({
+                                from: 'kalika.deltabee@gmail.com',
+                                to: email,
+                                subject: 'Welcome To Man2Help',
+                                html: 'Hello '+vandor_name+' !<br/> you are added as a Vendor. Your Visitor ID is <b>'+rows.insertId+'</b>. Please remember this id and show it to Security Officer on visit day<br>Thank You'
+
+                            }, function(error, response) {
+                                    if (error){
+                                        console.log(error);
+                                    }else{
+                                        console.log('Email sent');
+                                    }
+                            });
+                          
+                        var query = "delete from image_temp where id='"+id_proof+"'";
+                        pool.query(query, function(err, rows, fields) {
+                            if(err){
+                                console.log(err);
+                            }else{
+                                result.status = 200;
+                                res.send(JSON.stringify(result));
+                            }    
+                          });
+                        }    
+                    });
+                }else{
+                    result.error = "Select ID Image."
+                    res.send(JSON.stringify(result)); 
+                }
             }
-        }
-    });
-  }
+        });
+    }
 }
 
 exports.listvendors = function(pool){
