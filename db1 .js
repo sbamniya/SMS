8fce8fabbf5a5e43f37dfc16408bd1f64e7f5e91
sonu bamniya <sbamniya23@gmail.com>
2017-01-17 11:44:39 +0530

    Expense

--- db1 .js
+++
@@ -1,9 +0,0 @@
-var mySQL = require('mysql');
-    var pool  = mySQL.createPool({
-        host:  'localhost',
-        user:'root',
-        password:'',
-        database:'society_management'
-    });
-    
-    module.exports = pool;
--- societymanager.js
+++ societymanager.js
@@ -1,61 +1,60 @@
 exports.getmanagerList= function(pool){
-   return function(req,res){  
-       
-      var draw = req.query.draw;
-      var start = req.query.start;
-      var length = req.query.length;
-      var search_key = req.query.search.value;
-      var end = parseInt(start) + parseInt(length);
-      
-      var pageSize = length != null ? parseInt(length) : 0;
-      var skip = start != null ? parseInt(start) : 0;
-      var recordsTotal = 0;
+  return function(req,res){  
 
-      res.setHeader('Content-Type', 'application/json');
-      var result = {};
-      var query = "select * from  `society_manager`";
-      if(search_key!=''){
-        query +=' WHERE manager_name like "%'+search_key+'%" or email like "%'+search_key+'%"';
-      }
+    var draw = req.query.draw;
+    var start = req.query.start;
+    var length = req.query.length;
+    var search_key = req.query.search.value;
+    var end = parseInt(start) + parseInt(length);
 
-      query += " order by id desc";
-        pool.query(query, function(err, rows, fields){
-            if(err){
-                console.log(err);
-            }else{
-                result.draw = draw;
-                recordsTotal = rows.length;
-                result.recordsTotal = recordsTotal;
+    var pageSize = length != null ? parseInt(length) : 0;
+    var skip = start != null ? parseInt(start) : 0;
+    var recordsTotal = 0;
 
-                var resultData = []
-                resultData.push(rows.slice(skip, parseInt(skip)+parseInt(pageSize)));
-
-                result.recordsFiltered = recordsTotal;
-                result.success = JSON.stringify(resultData[0]);
-                res.send(JSON.stringify(result));
-                return;
-            }
-        });
-      }
-};
-exports.ActiveManagersList= function(pool){
-   return function(req,res){  
-       
-      
-      res.setHeader('Content-Type', 'application/json');
-      var result = {};
-      var query = "select * from  `society_manager` where status=1 order by id desc";
-     
-      pool.query(query, function(err, rows, fields){
-          if(err){
-              console.log(err);
-          }else{
-              result.success = JSON.stringify(rows);
-              res.send(JSON.stringify(result));
-              return;
-          }
-      });
+    res.setHeader('Content-Type', 'application/json');
+    var result = {};
+    var query = "select * from  `society_manager`";
+    if(search_key!=''){
+      query +=' WHERE manager_name like "%'+search_key+'%" or email like "%'+search_key+'%"';
     }
+
+    query += " order by id desc";
+    pool.query(query, function(err, rows, fields){
+      if(err){
+        console.log(err);
+      }else{
+        result.draw = draw;
+        recordsTotal = rows.length;
+        result.recordsTotal = recordsTotal;
+
+        var resultData = []
+        resultData.push(rows.slice(skip, parseInt(skip)+parseInt(pageSize)));
+
+        result.recordsFiltered = recordsTotal;
+        result.success = JSON.stringify(resultData[0]);
+        res.send(JSON.stringify(result));
+        return;
+      }
+    });
+  }
+};
+
+exports.ActiveManagersList= function(pool){
+  return function(req,res){  
+    res.setHeader('Content-Type', 'application/json');
+    var result = {};
+    var query = "select * from  `society_manager` where status=1 order by id desc";
+
+    pool.query(query, function(err, rows, fields){
+      if(err){
+        console.log(err);
+      }else{
+        result.success = JSON.stringify(rows);
+        res.send(JSON.stringify(result));
+        return;
+      }
+    });
+  }
 };
 
 
@@ -107,42 +106,41 @@ exports.addManager=function(pool,randomstring,crypto, transporter){
             else
             { 
               transporter.sendMail(
-                {
-                  from: 'kalika.deltabee@gmail.com',
-                  to: email,
-                  subject: 'Check  Id & Password',
-                  html: 'Hey '+manager_name+'<br/>Your id is: '+email+'<br/>Your password is: '+text+' '
+              {
+                from: 'kalika.deltabee@gmail.com',
+                to: email,
+                subject: 'Check  Id & Password',
+                html: 'Hey '+manager_name+'<br/>Your id is: '+email+'<br/>Your password is: '+text+' '
 
-                  }, function(error, response) {
-                  if (error) 
-                  {
-                    console.log(error);
-                  } 
-                  else 
-                  {
-                    console.log('Message sent');
-                  }
+              }, function(error, response) {
+                if (error) 
+                {
+                  console.log(error);
+                } 
+                else 
+                {
+                  console.log('Message sent');
+                }
 
               });
-             if(rows.insertId>0){
-                 var manager_id = rows.insertId;
-                 var merchant_id = req.body.merchant_id;
-                 var marchant_key = req.body.marchant_key;
-                 var marchant_salt = req.body.marchant_salt;
-                 var Q = 'INSERT INTO society_manager_meta(`merchant_id`, `marchant_key`, `marchant_salt`, `manager_id`, `status`) VALUES ("'+merchant_id+'","'+marchant_key+'","'+marchant_salt+'","'+manager_id+'","1")';
-          pool.query(Q, function(err, rows, fields){
-            if (err){
-              console.log(err);
-              result.error= err;
-                }
-            else{
-                  result.success="Manager Registered Successfully";
-                  res.send(JSON.stringify(result));   
-                  return;
-                }
-             });
-           };
-           }
+              if(rows.insertId>0){
+                var manager_id = rows.insertId;
+                var merchant_id = req.body.merchant_id;
+                var marchant_key = req.body.marchant_key;
+                var marchant_salt = req.body.marchant_salt;
+                var Q = 'INSERT INTO society_manager_meta(`merchant_id`, `marchant_key`, `marchant_salt`, `manager_id`, `status`) VALUES ("'+merchant_id+'","'+marchant_key+'","'+marchant_salt+'","'+manager_id+'","1")';
+                pool.query(Q, function(err, rows, fields){
+                  if (err){
+                    console.log(err);
+                    result.error= err;
+                  }else{
+                    result.success="Manager Registered Successfully";
+                    res.send(JSON.stringify(result));   
+                    return;
+                  }
+                });
+              };
+            }
           });
         };
       };
@@ -150,103 +148,95 @@ exports.addManager=function(pool,randomstring,crypto, transporter){
   };
 };
 exports.societyBlockList= function(pool){
- return function(req,res){  
-        sess=req.session;
-        var id =  sess.userID;
-        res.setHeader('Content-Type', 'application/json');
-        var result = {};
-        var query = "SELECT sm.name as society_name, bm.name as block_name, bm.id FROM block_master as bm INNER JOIN society_master as sm on sm.id=bm.parent_id where bm.block_manager='"+id+"' and bm.status= 1 "; 
-        pool.query(query, function(err, rows, fields){
-           if(err)
-           {
-              console.log(err);
-           }
-           else
-           {
-              result.success = rows;
-              res.send(JSON.stringify(result));
-              return;
-            }
-        });
-
-     };
-
+  return function(req,res){  
+    sess=req.session;
+    var id =  sess.userID;
+    res.setHeader('Content-Type', 'application/json');
+    var result = {};
+    var query = "SELECT sm.name as society_name, bm.name as block_name, bm.id FROM block_master as bm INNER JOIN society_master as sm on sm.id=bm.parent_id where bm.block_manager='"+id+"' and bm.status= 1 "; 
+    pool.query(query, function(err, rows, fields){
+      if(err)
+      {
+        console.log(err);
+      }
+      else
+      {
+        result.success = rows;
+        res.send(JSON.stringify(result));
+        return;
+      }
+    });
   };
-   exports.checkForSocietyManager= function(pool){
-           return function(req,res){  
-                    var managerId =req.body.managerId;
-                    var blockId =req.body.block_id;
-                    res.setHeader('Content-Type', 'application/json');
-                    var result = {};
-                    var query = "select sm.*, bm.id as block_id from  society_master sm INNER JOIN block_master bm on bm.parent_id=sm.id where sm.society_manager = '"+managerId+"' and bm.id = '"+blockId+"' and sm.status =1 "; 
+};
 
-              pool.query(query, function(err, rows, fields){
-                  if(err)
-                 {
-                    console.log('error');
-                  }
-                  
-                  else
-                  {
-                       if(rows.length>0)
-                       {
-                          result.is_societymanager = 1;
-                       }else{
-                          result.is_societymanager = 0;
-                       }
-                    
-                       res.send(JSON.stringify(result));
-                       return;
-                  }
-              
-              });
-           
-           };
+exports.checkForSocietyManager= function(pool){
+  return function(req,res){  
+    var managerId =req.body.managerId;
+    var blockId =req.body.block_id;
+    res.setHeader('Content-Type', 'application/json');
+    var result = {};
+    var query = "select sm.*, bm.id as block_id from  society_master sm INNER JOIN block_master bm on bm.parent_id=sm.id where sm.society_manager = '"+managerId+"' and bm.id = '"+blockId+"' and sm.status =1 "; 
 
-        };
+    pool.query(query, function(err, rows, fields){
+      if(err)
+      {
+        console.log('error');
+      }
+      else
+      {
+        if(rows.length>0)
+        {
+          result.is_societymanager = 1;
+        }else{
+          result.is_societymanager = 0;
+        }
+        res.send(JSON.stringify(result));
+        return;
+      }
+    });
+  };
+};
+
 exports.deleteManager = function(pool){
   return function(req,res){
     var id =  req.body.id;
     var result = {}
     pool.query("DELETE FROM society_manager WHERE id=?",[id],function(err, rows, fields){
-          if(err)
-          {
-              result.error = err;
-          }
-        else
-          {
-            result.success = "Manager deleted successfully";
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(result)); 
-          };  
-      });
-
+      if(err)
+      {
+        result.error = err;
+      }
+      else
+      {
+        result.success = "Manager deleted successfully";
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(result)); 
+      };  
+    });
   };
 };
-exports.updatePassword=  function(crypto,pool){
-        return function(req,res)
-        {
-            sess=req.session;
-            var id =sess.userID;
-            var newpass = req.body.pass;
-            var passwordn = crypto.createHash('md5').update(newpass).digest("hex");
-            var result = {};
-            var queryString = 'UPDATE society_manager SET  password ="'+passwordn+'",forget_token=""  where id = "'+id+'"';
-            
-            pool.query(queryString, function(err, rows, fields)  {
-                     if (err){
-                         result.error= err;
-                         res.setHeader('Content-Type', 'application/json');
-                         res.send(JSON.stringify(result));   
-                     }
-                     else
-                     {  
-                            result.succes = "Your Password has been changed successfully.";
-                            res.setHeader('Content-Type', 'application/json');
-                            res.send(JSON.stringify(result));       
-                     }
-              });
-        
 
+exports.updatePassword=  function(crypto,pool){
+  return function(req,res){
+    sess=req.session;
+    var id =sess.userID;
+    var newpass = req.body.pass;
+    var passwordn = crypto.createHash('md5').update(newpass).digest("hex");
+    var result = {};
+    var queryString = 'UPDATE society_manager SET  password ="'+passwordn+'",forget_token=""  where id = "'+id+'"';
+
+    pool.query(queryString, function(err, rows, fields)  {
+      if (err){
+        result.error= err;
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(result));   
+      }
+      else
+      {  
+        result.succes = "Your Password has been changed successfully.";
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(result));       
+      }
+    });
   };
 };
